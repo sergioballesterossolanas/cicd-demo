3533569c61d13216a0cf5948c36f52c4b0886362
sergioballesterossolanas <sergio.ballesteros@databricks.com>
2021-10-14 15:43:20 +0100

    my change

--- cicd_sample_project/jobs/sample/entrypoint.py
+++ cicd_sample_project/jobs/sample/entrypoint.py
@@ -15,7 +15,6 @@ class SampleJob(Job):
             self.logger.info("Init configuration is already provided")
 
     def launch(self):
-        print("Hello world!")
         self.logger.info("Launching sample job")
 
         listing = self.dbutils.fs.ls("dbfs:/")
